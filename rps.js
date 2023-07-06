675dac33d4f344faa2c68a180a811194017d0f3f
Zack <zackhoang2004@gmail.com>
2023-07-06 15:59:44 +0700

    Add playRound function

--- rps.js
+++ rps.js
@@ -1,7 +1,24 @@
+const choices = ['rock', 'paper', 'scissor'];  
 function getComputerChoice(choices) {
     return choices[Math.floor(Math.random()*choices.length)];
 }
-const choices = ['rock', 'paper', 'scissors']; 
-console.log(getComputerChoice(choices)); 
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return ('You Lose! Paper beats Rock!');
+    } else if (playerSelection === 'paper' && computerSelection == 'scissor') {
+        return ('You Lose! Scissor beats Paper!'); 
+    } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        return ('You Lose! Rock beats Scissor!')
+    } else if (playerSelection === computerSelection) {
+        return ('That\s a tie!')
+    } else {
+        return ('You Won!')
+    }
+}
+const playerSelection = 'paper'; 
+const computerSelection = getComputerChoice(choices);
+console.log("result: " + playRound(playerSelection, computerSelection));
+console.log(playerSelection)
+console.log(computerSelection)
 
